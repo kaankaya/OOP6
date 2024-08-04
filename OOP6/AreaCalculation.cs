3b34b972256f8fe14ee4dc7aca0b28efabc96057
Kaan Kaya <kaankaya.0707@gmail.com>
2024-08-04 17:45:48 +0300

    Add project files.

---
+++ OOP6/AreaCalculation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6
+{
+    //Base class
+    public abstract class AreaCalculation
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+
+        public virtual double Calculate()
+        {
+            return 0;
+        }
+        //Sonucu Yazdırmak için Ana Method
+        public void Display()
+        {
+            Console.WriteLine($"Alan Sonucu : {Calculate()}");
+        }
+
+    }
+    //Türemiş Sınıf
+    //derived class
+    public class Square : AreaCalculation
+    {
+        public override double Calculate()
+        {
+            return Width * Width;
+        }
+       
+    }//End Class Square
+
+    //Türemiş Sınıf
+    //derived class
+    public class Rectangle : AreaCalculation
+    {
+        public override double Calculate()
+        {
+            return Width * Height;
+        }
+    }//End Class Rectangle
+
+    //Türemiş Sınıf
+    //derived class
+    public class TriAngle : AreaCalculation
+    {
+        public override double Calculate()
+        {
+            return (Width * Height) / 2;
+        }
+    }//End Class TriAngle
+}
---
+++ OOP6/Program.cs
@@ -0,0 +1,21 @@
+﻿using OOP6;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Base class'a bağlı türemiş sınıfı işaret ederek sınıfı tanımlıyorum ve parametresini tanımlarken "{}" işaretlerini kullanarak veriyorum
+        AreaCalculation kare = new Square { Width = 5};
+        //display methodu calculate methodunu çalıştırdıgı için otomatik hesaplayıp sonucu ekrana yazıyor.Calculate methodu türemiş sınıflarda override edilerek kendi özgü işlem modellerini alabiliyor
+        kare.Display();
+        Console.WriteLine("*********************");
+
+        AreaCalculation dikdortgen = new Rectangle { Width = 4, Height = 5 };
+        dikdortgen.Display();
+        Console.WriteLine("**********************");
+
+        AreaCalculation dikUcgen = new TriAngle { Width = 4, Height = 5 };
+        dikUcgen.Display();
+        Console.WriteLine("**********************");
+    }
+}
